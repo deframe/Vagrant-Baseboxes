ef0cc53febe49c4d4ce173bfa32553540288fd21
David Frame <deframe@cryst.co.uk>
2012-12-16 21:10:16 +0000

    Small changes to VM Settings (smtp defaults, mysql username/password changes)

--- lamp/provision/vm-settings.sh
+++ lamp/provision/vm-settings.sh
@@ -7,12 +7,12 @@
 vm_smtp_server=smtp.gmail.com
 vm_smtp_use_tls=Yes
 vm_smtp_use_starttls=Yes
-vm_smtp_username=
-vm_smtp_password=
+vm_smtp_username=username@gmail.com
+vm_smtp_password=password
 
 # MySQL
 
-vm_mysql_root_password=lamp
+vm_mysql_root_password=dev
 
-vm_mysql_standard_user_username=lamp
-vm_mysql_standard_user_password=lamp
+vm_mysql_standard_user_username=dev
+vm_mysql_standard_user_password=dev
